7c014f0234051bd5fdd5a639251867ecf6784b2e
dongxl <1063273302@qq.com>
2019-10-29 12:50:34 +0800

    修改 ifneq ($(wildcard $(DEPLOY_BUILD_ANDROID_ARM64)), ) 判断 使其成功 就差 把注释的ztime.c 改好 src/ztime/ztime.c 文件中time_t timegm(struct tm* const t) 方法题内容注释 直接返回return -1

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -5,13 +5,13 @@ PROJDIR		:= $(PWD)
 
 DEPLOY_BUILD_ANDROID_ARM64 = $(PROJDIR)/build/android-arm64/lib/libavs.so
 
-ifneq ($(wildcard $(DEPLOY_BUILD_ANDROID_ARM64)), )
-$(info application.mk 2 android-arm64 no exist DEPLOY_BUILD_ANDROID_ARM64:$(DEPLOY_BUILD_ANDROID_ARM64)) 
-# APP_ABI := armeabi-v7a
+ifneq ($(wildcard $(DEPLOY_BUILD_ANDROID_ARM64)),)
+$(info application.mk 2 android-arm64 exist DEPLOY_BUILD_ANDROID_ARM64:$(DEPLOY_BUILD_ANDROID_ARM64)) 
 APP_ABI := armeabi-v7a arm64-v8a
 else
-$(info application.mk 1 android-arm64 exist DEPLOY_BUILD_ANDROID_ARM64:$(DEPLOY_BUILD_ANDROID_ARM64)) 
-APP_ABI := armeabi-v7a arm64-v8a
+$(info application.mk 1 android-arm64 no exist DEPLOY_BUILD_ANDROID_ARM64:$(DEPLOY_BUILD_ANDROID_ARM64)) 
+#APP_ABI := armeabi-v7a arm64-v8a
+APP_ABI := armeabi-v7a
 endif
 
 APP_PLATFORM := android-16
